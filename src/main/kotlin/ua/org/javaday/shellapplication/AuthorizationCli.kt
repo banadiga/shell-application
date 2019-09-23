171fd657f34df12adbe5498c8b864da04882a0c8
Ihor Banadiga <banadiga@gmail.com>
2019-09-23 14:09:04 +0300

    Improve AuthorizationCli and login

--- src/main/kotlin/ua/org/javaday/shellapplication/AuthorizationCli.kt
+++ src/main/kotlin/ua/org/javaday/shellapplication/AuthorizationCli.kt
@@ -1,14 +1,25 @@
 package ua.org.javaday.shellapplication
 
+import org.springframework.shell.standard.ShellCommandGroup
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
+import org.springframework.shell.standard.ShellOption
 import ua.org.ebank.clientbank.BankClient
 
 @ShellComponent
+@ShellCommandGroup("Authorization commands")
 class AuthorizationCli(val bankClient: BankClient) {
 
-    @ShellMethod("Login in to the bank.")
-    fun login(username: String, password: String) {
+    @ShellMethod("Login in to the bank.", key = ["login", "open"])
+    fun login(
+            @ShellOption(
+                    value = ["-l", "--login"],
+                    help = "Username"
+            )
+            username: String): String {
+        print("Password:")
+        val password = String(System.console().readPassword())
         bankClient.login(username, password)
+        return "Login successful"
     }
 }
