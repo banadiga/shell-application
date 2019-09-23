8b17aacc72c43e197f8e7d31cee8a64f4fbf0065
Ihor Banadiga <banadiga@gmail.com>
2019-09-23 14:03:21 +0300

    Add dependency on spring-shell-starter client-bank

--- src/main/kotlin/ua/org/javaday/shellapplication/ShellApplication.kt
+++ src/main/kotlin/ua/org/javaday/shellapplication/ShellApplication.kt
@@ -2,9 +2,15 @@ package ua.org.javaday.shellapplication
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import ua.org.ebank.clientbank.MyBankClient
 
 @SpringBootApplication
-class ShellApplication
+class ShellApplication {
+
+    @Bean
+    fun bankClient() = MyBankClient()
+}
 
 fun main(args: Array<String>) {
     runApplication<ShellApplication>(*args)
